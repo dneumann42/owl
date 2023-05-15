2d6a34b5443bdfcf275cece7fd1bd0a96fead100
dneumann <dneumann@mhighpoint.com>
2023-05-14 21:21:13 -0700

    cleanup and more tests

--- src/transpiler/mod.rs
+++ src/transpiler/mod.rs
@@ -1,6 +1,6 @@
 use crate::{
     parser::{parse, ParseError},
-    values::{Assignment, Do, List, Val},
+    values::{Assignment, Do, List, Table, Val},
 };
 
 pub type Result<T> = core::result::Result<T, TranspilerError>;
@@ -27,6 +27,96 @@ fn get_expr_from_stmt(v: &Val) -> Result<LuaSource> {
     }
 }
 
+fn transpile_table(tbl: &Table) -> Result<LuaSource> {
+    let mut ls: String = "".to_owned();
+
+    ls.push_str("{");
+
+    let mut it = tbl.arr.iter().peekable();
+
+    while let Some(v) = it.next() {
+        if v.is_stmt() {
+            ls.push_str(&fnwrap(v.to_lua()?))
+        } else {
+            ls.push_str(&v.to_lua()?)
+        }
+
+        ls.push_str(",")
+    }
+
+    let mut kv_it_vec: Vec<_> = tbl.kv.iter().collect();
+    kv_it_vec.sort_by(|(a, _), (b, _)| a.partial_cmp(b).unwrap());
+
+    let mut kv_it = kv_it_vec.iter().peekable();
+
+    while let Some((k, v)) = kv_it.next() {
+        ls.push_str(&format!("{} = {},", k, v.to_lua()?))
+    }
+
+    ls.push_str("}");
+
+    Ok(ls)
+}
+
+fn transpile_binop(op: &String, lh: &Box<Val>, rh: &Box<Val>) -> Result<LuaSource> {
+    Ok(format!(
+        "({} {} {})",
+        lh.as_ref().to_lua()?,
+        op,
+        rh.as_ref().to_lua()?
+    ))
+}
+
+fn transpile_do(xs: &Vec<Box<Val>>) -> Result<LuaSource> {
+    let mut ls: String = "".to_owned();
+
+    // turn it into a list of tuple(val, luastring)
+
+    let vs: Vec<Val> = xs.iter().map(|v| v.as_ref().to_owned()).collect();
+    let mut it = vs.iter().peekable();
+
+    while let Some(exp) = it.next() {
+        let mut val = "".to_owned();
+
+        if exp.is_stmt() {
+            ls.push_str(&exp.to_lua()?);
+            val = get_expr_from_stmt(exp)?;
+        } else {
+            val = exp.to_lua()?;
+        }
+
+        if it.peek().is_none() {
+            ls.push_str(&format!("return {}\n", val));
+        } else {
+            ls.push_str(&format!("local _ = {}\n", val));
+        }
+    }
+
+    Ok(fnwrap(ls))
+}
+
+fn transpile_block(xs: &Vec<Box<Val>>) -> Result<LuaSource> {
+    let mut ls: String = String::from("do\n");
+
+    for val in xs {
+        ls.push_str(&val.as_ref().to_lua()?);
+        ls.push_str("\n");
+    }
+
+    ls.push_str("end\n");
+
+    Ok(ls)
+}
+
+fn transpile_assignment(ident: &Box<Val>, expr: &Box<Val>) -> Result<LuaSource> {
+    let mut ls: String = String::from("local ");
+    ls.push_str(&ident.to_lua()?);
+    ls.push_str(" = ");
+    ls.push_str(&expr.to_lua()?);
+    ls.push_str("\n");
+    Ok(ls)
+}
+
 impl ToLua for Val {
     fn to_lua(&self) -> Result<LuaSource> {
         match self {
@@ -34,89 +124,11 @@ impl ToLua for Val {
             Val::Ident(a) => Ok(a.to_string()),
             Val::List(_) => todo!(),
             Val::UnOp((op, v)) => Ok(format!("{}{}", op, v.as_ref().to_lua()?)),
-            Val::BinOp((op, lh, rh)) => Ok(format!(
-                "({} {} {})",
-                lh.as_ref().to_lua()?,
-                op,
-                rh.as_ref().to_lua()?
-            )),
-            Val::Table(tbl) => {
-                let mut ls: String = "".to_owned();
-
-                ls.push_str("{");
-
-                let mut it = tbl.arr.iter().peekable();
-
-                while let Some(v) = it.next() {
-                    if v.is_stmt() {
-                        ls.push_str(&fnwrap(v.to_lua()?))
-                    } else {
-                        ls.push_str(&v.to_lua()?)
-                    }
-
-                    ls.push_str(",")
-                }
-
-                let mut kv_it_vec: Vec<_> = tbl.kv.iter().collect();
-                kv_it_vec.sort_by(|(a, _), (b, _)| a.partial_cmp(b).unwrap());
-
-                let mut kv_it = kv_it_vec.iter().peekable();
-
-                while let Some((k, v)) = kv_it.next() {
-                    ls.push_str(&format!("{} = {},", k, v.to_lua()?))
-                }
-
-                ls.push_str("}");
-
-                Ok(ls)
-            }
-            Val::Do(xs) => {
-                let mut ls: String = "".to_owned();
-
-                // turn it into a list of tuple(val, luastring)
-
-                let vs: Vec<Val> = xs.iter().map(|v| v.as_ref().to_owned()).collect();
-                let mut it = vs.iter().peekable();
-
-                while let Some(exp) = it.next() {
-                    let mut val = "".to_owned();
-
-                    if exp.is_stmt() {
-                        ls.push_str(&exp.to_lua()?);
-                        val = get_expr_from_stmt(exp)?;
-                    } else {
-                        val = exp.to_lua()?;
-                    }
-
-                    if it.peek().is_none() {
-                        ls.push_str(&format!("return {}\n", val));
-                    } else {
-                        ls.push_str(&format!("local _ = {}\n", val));
-                    }
-                }
-
-                Ok(fnwrap(ls))
-            }
-            Val::Block(xs) => {
-                let mut ls: String = String::from("do\n");
-
-                for val in xs {
-                    ls.push_str(&val.as_ref().to_lua()?);
-                    ls.push_str("\n");
-                }
-
-                ls.push_str("end\n");
-
-                Ok(ls)
-            }
-            Val::Assignment((ident, expr)) => {
-                let mut ls: String = String::from("local ");
-                ls.push_str(&ident.to_lua()?);
-                ls.push_str(" = ");
-                ls.push_str(&expr.to_lua()?);
-                ls.push_str("\n");
-                Ok(ls)
-            }
+            Val::BinOp((op, lh, rh)) => transpile_binop(op, lh, rh),
+            Val::Table(tbl) => transpile_table(tbl),
+            Val::Do(xs) => transpile_do(xs),
+            Val::Block(xs) => transpile_block(xs),
+            Val::Assignment((ident, expr)) => transpile_assignment(ident, expr),
             Val::Str(s) => Ok(format!("{:?}", s)),
             v => Ok(v.to_string()),
         }
--- tests/transpiler.rs
+++ tests/transpiler.rs
@@ -6,26 +6,33 @@ mod transpiler_tests {
     use trim_margin::MarginTrimmable;
 
     trait XX {
+        fn tr(self) -> String;
         fn xx(self) -> String;
     }
 
     impl XX for &str {
-        fn xx(self) -> String {
+        fn tr(self) -> String {
             self.trim_margin().unwrap().to_owned()
+        }
+
+        fn xx(self) -> String {
+            format!(
+                "
+                |(function()
+                |return {}
+                | end)()",
+                self
+            )
+            .trim_margin()
+            .unwrap()
+            .to_owned()
         }
     }
 
     #[test]
     fn it_can_transpile_numbers() {
         let x = "123".to_lua().unwrap();
-        assert_eq!(
-            x,
-            "
-            |(function()
-            |return 123
-            | end)()"
-                .xx()
-        );
+        assert_eq!(x, "123".xx());
 
         let x2 = "123 321".to_lua().unwrap();
         assert_eq!(
@@ -35,7 +42,7 @@ mod transpiler_tests {
             |local _ = 123
             |return 321
             | end)()"
-                .xx()
+                .tr()
         )
     }
 
@@ -43,51 +50,34 @@ mod transpiler_tests {
     fn it_can_transpile_nested_expressions() {
         assert_eq!(
             "(3 * (2 - a)) + (2 / 2)".to_lua().unwrap(),
-            "
-            |(function()
-            |return ((3 * (2 - a)) + (2 / 2))
-            | end)()"
-                .xx()
+            "((3 * (2 - a)) + (2 / 2))".xx()
         );
         assert_eq!(
             "(3 * (2 - a)) + 2 / 3.124".to_lua().unwrap(),
-            "
-            |(function()
-            |return ((3 * (2 - a)) + (2 / 3.124))
-            | end)()"
-                .xx()
+            "((3 * (2 - a)) + (2 / 3.124))".xx()
         )
     }
 
     #[test]
     fn it_can_transpile_tables() {
-        assert_eq!(
-            "{1 2 3}".to_lua().unwrap(),
-            "
-            |(function()
-            |return {1,2,3,}
-            | end)()"
-                .xx()
-        );
+        assert_eq!("{1 2 3}".to_lua().unwrap(), "{1,2,3,}".xx());
 
-        assert_eq!(
-            "{ a = 123 }".to_lua().unwrap(),
-            "
-            |(function()
-            |return {a = 123,}
-            | end)()"
-                .xx()
-        );
+        assert_eq!("{ a = 123 }".to_lua().unwrap(), "{a = 123,}".xx());
 
         assert_eq!(
             "{ a = 123
                b = test
-               1 2 3 }".to_lua().unwrap(),
-            "
-            |(function()
-            |return {1,2,3,a = 123,b = test,}
-            | end)()"
-                .xx()
+               1 2 3 }"
+                .to_lua()
+                .unwrap(),
+            "{1,2,3,a = 123,b = test,}".xx()
+        );
+
+        assert_eq!(
+            "{ a = { b = 420 } }"
+                .to_lua()
+                .unwrap(),
+            "{a = {b = 420,},}".xx()
         );
     }
 }
