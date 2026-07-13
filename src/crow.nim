8a45e185cafe7867a22ecd2eff29072c307217cb
dneumann42 <dneumann42@proton.me>
2026-07-13 07:03:18 -0700

    wip

--- src/crow.nim
+++ src/crow.nim
@@ -1,12 +1,19 @@
+import std/[os]
 import crow/[parser, evaluator]
 export parser
 
-let Content = readFile "scripts/repl.nest"
+let ReplContent = readFile "scripts/repl.nest"
 
 proc start() =
+  let cmds = commandLineParams()
+  var content = ReplContent
+  if cmds[0] == "run":
+    let path = cmds[1]
+    content = readFile path
+
   var evaluator = Evaluator.init()
   try:
-    let ast = parse Content
+    let ast = parse content
     discard evaluator.exec(ast)
   except CatchableError as error:
     quit error.msg, 1
